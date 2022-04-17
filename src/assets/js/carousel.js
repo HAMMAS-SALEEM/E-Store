14aa49d961a589de1e6affe4d5fc92d4017d7c1f
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-17 10:49:32 +0500

    :lipstick: Added Carousel

---
+++ src/assets/js/carousel.js
@@ -0,0 +1,58 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const prevBtn = document.querySelector('.carousel__btn--left');
+const nextBtn = document.querySelector('.carousel__btn--right');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+slides.forEach((img, idx) => {
+  img.style.left = `${slideWidth * idx}px`;
+});
+
+// const images = [
+//   { src: 'https://i.pinimg.com/originals/c2/8b/c9/c28bc99dafaeb107246f904f34bae16c.jpg' },
+//   { src: 'https://images.wallpaperscraft.com/image/single/space_galaxy_shine_137572_1280x720.jpg' },
+//   { src: 'https://cdn.wallpapersafari.com/74/30/v23HkS.jpg' },
+//   { src: 'https://wallpaperaccess.com/full/3971879.jpg' },
+// ];
+
+// window.onload = () => {
+//   images.forEach((img, idx) => {
+//     let firstClass = '';
+//     if (idx === 0) {
+//       firstClass = 'current_slide';
+//     }
+//     track.innerHTML += `
+// <li class="carousel__slide">
+// <img class="carousel__image ${firstClass}" src=${img.src} alt="carousel image">
+// </li>`;
+//   });
+// };
+
+const moveSlide = (track, currentSlide, targetSlide) => {
+  const amountToSlide = targetSlide.style.left;
+  track.style.transform = `translateX(-${amountToSlide})`;
+  currentSlide.classList.remove('current__slide');
+  targetSlide.classList.add('current__slide');
+  if (targetSlide === slides[0]) {
+    prevBtn.classList.add('is-hidden');
+    nextBtn.classList.remove('is-hidden');
+  } else if (targetSlide === slides[slides.length - 1]) {
+    prevBtn.classList.remove('is-hidden');
+    nextBtn.classList.add('is-hidden');
+  } else {
+    prevBtn.classList.remove('is-hidden');
+    nextBtn.classList.remove('is-hidden');
+  }
+};
+
+prevBtn.addEventListener('click', () => {
+  const currentSlide = track.querySelector('.current__slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  moveSlide(track, currentSlide, prevSlide);
+});
+
+nextBtn.addEventListener('click', () => {
+  const currentSlide = track.querySelector('.current__slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  moveSlide(track, currentSlide, nextSlide);
+});
